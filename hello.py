777be93b2266797a20d3ebba58dca144ebf90dea
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-04-20 20:40:12 +0300

    clean whitespace from str

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-# ask user for their name 
+# Ask user for their name 
 name = input("what's your name? ")
 
-# say hello to user
+# Remove whitespace from str
+name = name.strip()
+
+# Say hello to user
 print(f"hello, {name}")
