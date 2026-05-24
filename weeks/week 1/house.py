b6b61b727245ca50972a792582cbdbe4fcbf808b
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-05-24 09:02:09 +0300

    3 elif to 1 if

--- weeks/week 1/house.py
+++ weeks/week 1/house.py
@@ -1,10 +1,6 @@
 name = input("What's your name? ")
 
-if name == "Harry":
-    print("Griffindor")
-elif name == "Hermione":
-    print("Griffindor")
-elif name == "Ron":
+if name == "Harry" or name == "Hermione" or name == "Ron":
     print("Griffindor")
 elif name == "Draco":
     print("Slytherin")
