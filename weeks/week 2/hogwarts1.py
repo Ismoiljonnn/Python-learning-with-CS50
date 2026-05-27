62c420ad21a3491828321900dd3183dbb89e0b1e
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-05-27 12:17:25 +0300

    small dict

---
+++ weeks/week 2/hogwarts1.py
@@ -0,0 +1,9 @@
+students = {
+  "Hermione": "Griffindor", 
+  "Harry": "Griffindor", 
+  "Ron": "griffindor", 
+  "Draco": "Slytherin"
+}
+
+for student in students:
+  print(student, student[student], sep=", ")
