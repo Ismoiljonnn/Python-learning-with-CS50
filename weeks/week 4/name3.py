fe0155b85258e5b96fc4d50eb2857c2cef36d6e1
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-05-29 10:35:21 +0300

    updating

--- weeks/week 4/name3.py
+++ weeks/week 4/name3.py
@@ -3,6 +3,6 @@ import sys
 if len(sys.argv) < 2:
     print("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too few arguments")
+    print("Too many arguments")
 else:
     print("hello, my name is", sys.argv[1])
