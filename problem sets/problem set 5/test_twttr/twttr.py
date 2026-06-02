2a1830aa43ef46f569344b610febc08da0028a95
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-02 05:40:15 +0300

    for test

---
+++ problem sets/problem set 5/test_twttr/twttr.py
@@ -0,0 +1,14 @@
+def main():
+  text = input("Input: ")
+  result = ""
+
+  vowels = "aeiouAEIOU"
+
+  for char in text:
+    if char not in vowels:
+      result += char
+
+  print("Output: ", result)
+
+if __name__ == "__main__":
+  main()
