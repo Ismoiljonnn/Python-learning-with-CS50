dd6fd664ef8f24fb825f194e9485bbff18fd9537
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-02 06:03:15 +0300

    added test numbers and punctuation

--- problem sets/problem set 5/test_twttr/test_twttr.py
+++ problem sets/problem set 5/test_twttr/test_twttr.py
@@ -5,3 +5,9 @@ def test_shorten():
   assert shorten("twitter") == "twttr"
   assert shorten("HELLO") == "HLL"
   assert shorten("apple") == "ppl"
+
+def test_numbers():
+  assert shorten("1234") == "1234"
+
+def test_punctuation():
+  assert shorten("!?.") == "!?."
