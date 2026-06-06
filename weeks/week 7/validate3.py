22d7aeeef98dcbb03f6e6df5a1ba88cb7852329a
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-06 14:12:16 +0300

    added (\w+\.)? after the @ then it supports subdomains

--- weeks/week 7/validate3.py
+++ weeks/week 7/validate3.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w.+@\w+\.\w+\.edu$", re.IGNORECASE):
+if re.search(r"^\w.+@(\w+\.)?\w+\.edu$", re.IGNORECASE):
   print("Valid")
 else:
   print("Invalid")
