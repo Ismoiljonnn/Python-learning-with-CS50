4027d096744cde421d5885986004fe6b84b71729
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-06 12:03:02 +0300

    changing input to lowercase

--- weeks/week 7/validate2.py
+++ weeks/week 7/validate2.py
@@ -1,8 +1,8 @@
 import re
 
-email = input("What's your email? ").strip()
+email = input("What's your email? ").strip().lower()
 
 if re.search(r"^\w.+@[a-zA-Z0-9_]+\.edu$", email):
   print("Valid")
 else:
-  print("Invalid") 
+  print("Invalid")
