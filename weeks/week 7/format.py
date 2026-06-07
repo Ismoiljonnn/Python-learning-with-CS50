511122c7149ba3a38d21d131ab8424ed658c0f73
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-07 14:35:21 +0300

    using re for name formating

--- weeks/week 7/format.py
+++ weeks/week 7/format.py
@@ -1,7 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), ?(.+)$", name)
 if matches:
   last = matches.group(1)
   first = matches.group(2)
