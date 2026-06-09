f57b9af1092e5ddfc92f64139dd2b352d89b3ac6
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-09 09:54:08 +0300

    numbers task completed

--- problem sets/problem set 7/numb3rs.py
+++ problem sets/problem set 7/numb3rs.py
@@ -2,17 +2,15 @@ import re
 import sys
 
 def main():
-  print(validate(input("IPv4 Adress: ")))
+    print(validate(input("IPv4 Address: ")))
         
 def validate(ip):
-  pattern = r"^([0-9][1-9][0-9]1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))$"
-
-
-  if re.search(pattern, ip):
-    return True
-  else:
+    octet = r"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+    pattern = rf"^{octet}\.{octet}\.{octet}\.{octet}$"
+    
+    if re.search(pattern, ip):
+        return True
     return False
 
-
 if __name__ == "__main__":
-  main()
+    main()
