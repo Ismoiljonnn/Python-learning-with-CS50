c5a55c0b2e55262993c056a09e79e6ca9c21b2f2
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-09 17:36:32 +0300

    working 9 to 5 test completed

--- problem sets/problem set 7/working.py
+++ problem sets/problem set 7/working.py
@@ -2,26 +2,27 @@ import re
 import sys
 
 def main():
-    print(convert(input("Hours: ")))
+  print(convert(input("Hours: ")))
 
 def convert(s):
-    # Regex pattern: (soat):(daqiqa) (AM/PM) to (soat):(daqiqa) (AM/PM)
-    pattern = r"^(1[0-2]|[1-9]):([0-5][0-9]) (AM|PM) to (1[0-2]|[1-9]):([0-5][0-9]) (AM|PM)$"
-    
-    match = re.search(pattern, s)
-    if match:
-        h1, m1, ap1, h2, m2, ap2 = match.groups()
-        return f"{time_format(h1, m1, ap1)} to {time_format(h2, m2, ap2)}"
-    
-    raise ValueError
+  pattern = r"^(1[0-2]|[1-9])(?::([0-5][0-9]))? (AM|PM) to (1[0-2]|[1-9])(?::([0-5][0-9]))? (AM|PM)$"
+  
+  match = re.search(pattern, s)
+  if match:
+      h1, m1, ap1, h2, m2, ap2 = match.groups()
+      m1 = m1 if m1 else "00"
+      m2 = m2 if m2 else "00"
+      return f"{time_format(h1, m1, ap1)} to {time_format(h2, m2, ap2)}"
+  
+  raise ValueError
 
 def time_format(h, m, ap):
-    h = int(h)
-    if ap == "PM" and h != 12:
-        h += 12
-    elif ap == "AM" and h == 12:
-        h = 0
-    return f"{h:02}:{m}"
+  h = int(h)
+  if ap == "PM" and h != 12:
+      h += 12
+  elif ap == "AM" and h == 12:
+      h = 0
+  return f"{h:02}:{m}"
 
 if __name__ == "__main__":
-    main()
+  main()
