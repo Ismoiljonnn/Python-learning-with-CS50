e20b3beaed44f4b3ad6fd666d19aa85982cf22a1
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-12 15:07:20 +0300

    weasley and total added

--- weeks/week 8/vault.py
+++ weeks/week 8/vault.py
@@ -10,3 +10,14 @@ class Vault:
 
 potter = Vault(100, 50, 25)
 print(potter)
+
+
+weasley = Vault(25, 50, 100)
+print(weasley)
+
+galleons = potter.galleons + weasley.galleons
+sickles = potter.sickles + weasley.sickles
+knuts = potter.knuts + weasley.knuts
+
+total = Vault(galleons, sickles, knuts)
+print(total)
