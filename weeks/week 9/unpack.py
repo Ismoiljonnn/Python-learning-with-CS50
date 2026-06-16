bc9d1ed1a04d3b56d03cb36bffe82f703d1a354b
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-06-16 09:40:22 +0300

    updating like galleons, sickles, knuts calculator

--- weeks/week 9/unpack.py
+++ weeks/week 9/unpack.py
@@ -1,2 +1,4 @@
-first, _ = input("what's your name? ").split(" ")
-print(f"hello, {first}")
+def total(galleons, sickles, knuts):
+  return (galleons * 17 + sickles) * 29 + knuts
+
+total(100, 50, 25)
